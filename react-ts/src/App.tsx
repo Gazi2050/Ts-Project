1b860fe75b4e47bb516f82efe169dbf5a1a38459
Gazi2050 <gazi45344@gmail.com>
2024-05-03 16:44:39 +0600

    set-up client site

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="mt-24">
+      <h1 className="text-center text-4xl font-bold">Hello React-TypeScript</h1>
+    </div>
+  )
+}
+
+export default App
