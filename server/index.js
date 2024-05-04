cb75db9e7c4ba93d38c3d9bed35e892e04029de5
Gazi2050 <gazi45344@gmail.com>
2024-05-04 21:58:46 +0600

    update server

--- server/index.js
+++ server/index.js
@@ -36,6 +36,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/blogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await blogCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post('/blogs', async (req, res) => {
             const blog = req.body;
             // console.log(blog);
